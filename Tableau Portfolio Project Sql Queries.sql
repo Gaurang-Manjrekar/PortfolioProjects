3ef90ef5af5f233041d0b49187c2173eaddf20ca
Gaurang Nikhil Manjrekar <115617589+Gaurang-Manjrekar@users.noreply.github.com>
2023-04-14 00:03:38 +0530

    Update Tableau Portfolio Project Sql Queries.sql

--- Tableau Portfolio Project Sql Queries.sql
+++ Tableau Portfolio Project Sql Queries.sql
@@ -15,8 +15,6 @@ where continent is not null
 --Group By date
 order by 1,2
 
--- Just a double check based off the data provided
--- numbers are extremely close so we will keep them - The Second includes "International"  Location
 
 
 --Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
@@ -29,8 +27,7 @@ order by 1,2
 
 -- 2. 
 
--- We take these out as they are not inluded in the above queries and want to stay consistent
--- European Union is part of Europe
+
 
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths_1$
@@ -64,16 +61,6 @@ order by PercentPopulationInfected desc
 
 
 
-
-
-
-
-
-
--- Queries I originally had, but excluded some because it created too long of video
--- Here only in case you want to check them out
-
-
 -- 1.
 
 Select dea.continent, dea.location, dea.date, dea.population
@@ -99,8 +86,6 @@ where continent is not null
 order by 1,2
 
 
--- Just a double check based off the data provided
--- numbers are extremely close so we will keep them - The Second includes "International"  Location
 
 
 --Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
@@ -112,9 +97,6 @@ order by 1,2
 
 
 -- 3.
-
--- We take these out as they are not inluded in the above queries and want to stay consistent
--- European Union is part of Europe
 
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
 From PortfolioProject..CovidDeaths_1$
